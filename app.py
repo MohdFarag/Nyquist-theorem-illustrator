875b1eef40a989b0819532327196fba03777f2d2
mohdfarag <mohamed.mahmoud02@eng-st.cu.edu.eg>
2022-03-14 00:55:44 +0200

    small changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # !/usr/bin/python
 
 # import plotter.py class
-import plotter
+from plotter import Plot
 
 # importing Qt widgets
 from PyQt5.QtWidgets import *
@@ -17,6 +17,7 @@ import pandas as pd
 import pyqtgraph as pg
 from pyqtgraph.dockarea import *
 
+import sys
 
 class Window(QMainWindow):
     """Main Window."""
@@ -24,3 +25,73 @@ class Window(QMainWindow):
         
         """Initializer."""
         super().__init__()
+
+        self.setWindowIcon(QIcon('images/icon.png'))
+        
+        # setting title
+        self.setWindowTitle("Nyquist Theorem Illustrator")
+
+        # UI contents
+        self.createMenuBar()
+        self.createtoolBar()
+        self.initUI()
+        
+        # Status Bar
+        self.statusBar = QStatusBar()
+        self.statusBar.setStyleSheet("font-size:13px; padding: 3px; color: black; font-weight:900")
+        self.statusBar.showMessage("Welcome to our application...")
+        self.setStatusBar(self.statusBar)
+
+    # Menu
+    def createMenuBar(self):
+
+        # MenuBar
+        menuBar = self.menuBar()
+        
+        # Creating menus using a QMenu object
+        fileMenu = QMenu("&File", self)
+        
+        openFile = QAction("Open...",self)
+        openFile.setShortcut("Ctrl+o")
+        openFile.setStatusTip('Open a new signal')
+        openFile.triggered.connect(self.browseSignal)
+
+        fileMenu.addAction(openFile)
+
+        quit = QAction("Exit",self)
+        quit.setShortcut("Ctrl+q")
+        quit.setStatusTip('Exit application')
+        quit.triggered.connect(self.exit)
+        
+        fileMenu.addAction(quit)
+
+        # Add file tab to the menu
+        menuBar.addMenu(fileMenu)
+
+    def createtoolBar(self):
+        pass
+    # GUI
+    def initUI(self):
+        wid = QWidget(self)
+        self.setCentralWidget(wid)
+        # setting configuration options
+        pg.setConfigOptions(antialias=True)
+
+        # Outer Layout
+        outerLayout = QVBoxLayout()
+
+        ######### INIT GUI #########
+
+        
+
+
+        # outerLayout.addWidget()
+        ######### INIT GUI #########
+
+        wid.setLayout(outerLayout)
+
+    def browseSignal(self):
+        pass
+
+    def exit(self):
+        sys.exit()
--- plotter.py
+++ plotter.py
@@ -12,9 +12,29 @@ import pandas as pd
 # importing pyqtgraph as pg
 import pyqtgraph as pg
 
-class Plot(pg):
+class Plot(pg.GraphicsLayoutWidget):
     """Main Plot."""
-    def __init__(self):
+    def __init__(self,title="Signal Plot"):
         
         """Initializer."""
         super().__init__()
+        self.plot = self.addPlot()
+
+        self.plot.setTitle(title, size="20pt")
+        self.plot.setLabel('bottom', 'Time', 's')
+        self.plot.showGrid(x=True, y=True)
+
+    def plotContinuousSignal(self, frequency, magnitude, phase):
+        tMin = -np.pi/2
+        tMax = np.pi/2
+        t = np.linspace(tMin, tMax, 1000)
+        y = magnitude * np.sin(2 * np.pi * frequency * t + phase)
+        self.plot.plot(t, y)
+        
+        return(y)
+
+    def plotDiscreteSignal(self, y, x=np.linspace(-5, 5, 1000)):
+        self.plot.plot(x, y)
+
+    def clearPlot(self):
+        self.plot.clear()
